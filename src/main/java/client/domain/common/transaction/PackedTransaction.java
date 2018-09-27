24c53983fe21fa3caa9bdbd61895d67d46bdc06e
drbillnguyen <33407019+drbillnguyen@users.noreply.github.com>
2018-09-27 13:44:46 +0700

    add ignore field for pojo

--- src/main/java/client/domain/common/transaction/PackedTransaction.java
+++ src/main/java/client/domain/common/transaction/PackedTransaction.java
@@ -1,11 +1,13 @@
 package client.domain.common.transaction;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PackedTransaction {
 
